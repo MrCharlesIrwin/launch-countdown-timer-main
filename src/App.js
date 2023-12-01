5b06169bc78b47fb6943e64e699c991b2cd7f2dd
Charles <116520612d@gmail.com>
2023-12-01 19:35:50 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { add, getDay } from "date-fns";
+
 function App() {
-  const [countDownStart, setCountDownStart] = useState({
-    days: 8,
-    hours: 23,
-    minutes: 55,
-    seconds: 41,
-  });
+  const [days, setDays] = useState("1");
+  const [prevdays, setPrevDays] = useState("2");
+  const [hours, setHours] = useState("2");
+  const [prevhours, setPrevHours] = useState("3");
+  const [minutes, setMinutes] = useState("11");
+  const [prevMinutes, setPrevMinutes] = useState("12");
+  const [seconds, setSeconds] = useState("1");
+  const [prevSecond, setPrevSecond] = useState("2");
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCountDownStart({ ...countDownStart, seconds: countDownStart.seconds - 1 });
-    }, 1000);
+  const [minutesAnimation, setMinutesAnimation] = useState(false);
+  const [hoursAnimation, setHoursAnimation] = useState(false);
+  const [daysAnimation, setDaysAnimation] = useState(false);
 
-    if (countDownStart.seconds === 0) {
-      setCountDownStart({
-        ...countDownStart,
-        minutes: countDownStart.minutes - 1,
-        seconds: 60,
-      });
+  function secondsHandler() {
+    if (seconds === 0) {
+      setMinutesAnimation(true);
+      setMinutes(minutes - 1);
+      setPrevMinutes(prevMinutes - 1);
+      setSeconds(59);
+      return setPrevSecond(0);
     }
-    if (countDownStart.minutes === 0) {
-      setCountDownStart({
-        ...countDownStart,
-        hours: countDownStart.hours - 1,
-        minutes: 60,
-      });
+    if (seconds === 59) {
+      setSeconds(58);
+      return setPrevSecond(59);
     }
-    if (countDownStart.hours === 0) {
-      setCountDownStart({
-        ...countDownStart,
-        days: countDownStart.days - 1,
-        hours: 24,
-      });
-    }
-  }, [countDownStart]);
 
-  //  useEffect(()=>{
-  //   setTimeout(()=>{
-  //     setSeconds((countToDate.getSeconds() - today.getSeconds())-1)
-  //   },1000)
-  //  })
-  const nd = add(new Date(), {
-    days: 1,
-    hours: 1,
-    minutes: 1,
-    seconds: 30,
-  });
-  const [days, setDays] = useState(1);
-  const [hours, setHours] = useState(2);
-  const [minutes, setMinutes] = useState(11);
-  const [seconds, setSeconds] = useState(42);
- const [prevSecond,setPrevSecond] = useState(43)
-  function cs() {
-    setSeconds(seconds - 1);
-    setPrevSecond(prevSecond-1)
+    setSeconds(seconds - "1");
+    setPrevSecond(prevSecond - "1");
   }
-  
+  function minutesHandler() {
+    if (minutes === 0) {
+      setHoursAnimation(true);
+      setHours(hours - 1);
+      setMinutes(59);
+      return setPrevMinutes(0);
+    }
+    if (minutes === 59) {
+      setMinutes(58);
+      return setPrevMinutes(59);
+    }
+  }
+  function hourHandler() {
+    if (hours === 0) {
+      setDaysAnimation(true);
+      setDays(days - 1);
+      setHours(59);
+      return setPrevHours(0);
+    }
+    if (hours === 59) {
+      setHours(58);
+      return setPrevHours(59);
+    }
+  }
+  function daysHandler() {
+    if (days === 0) return;
+  }
 
   return (
     <div className="App">
@@ -65,26 +65,81 @@ function App() {
         <h1>We're launching soon</h1>
         <section className="flipClock">
           <div className="flipClockCards">
-            <div className="top">{countDownStart.days}</div>
-            <div className="bottom">{countDownStart.days}</div>
+            <div className="topHalfFlipCard">
+              <div>{days}</div>
+            </div>
+            <div className={`topHalfAnimatedFlipCardCover ${daysAnimation === false ? "animationNone" : ""} `} key={prevdays}>
+              <div>{prevdays}</div>
+            </div>
+            <div className="bottomHalfFlipCard">
+              <div>{prevdays}</div>
+            </div>
+            <div
+              className={`bottomHalfAnimatedFlipCardCover ${daysAnimation === false ? "animationNone" : ""}`}
+              onAnimationEnd={daysHandler}
+              key={days}
+            >
+              <div>{days}</div>
+            </div>
           </div>
-          <div className="flipClockCards">
-            <div className="top">{countDownStart.hours}</div>
-            <div className="bottom">{countDownStart.hours}</div>
-          </div>
-          <div className="flipClockCards">
-            <div className="top">{countDownStart.minutes}</div>
-            <div className="bottom">{countDownStart.minutes}</div>
-          </div>
-          <div className="flipClockCards">
-            {/* <div className="topUnder" onAnimationStart={() => seconds}> */}
-            {/* <div key={seconds} className="top"  onAnimationEnd={cs}> */}
 
-            <div className="topHalfFlipCard" >{seconds}</div>
-            <div className="topHalfAnimatedFlipCardCover" key={prevSecond}>{prevSecond}</div>
-            <div className="bottomHalfFlipCard">{seconds}</div>
-            <div className="bottomHalfAnimatedFlipCardCover" onAnimationEnd={cs}  >{prevSecond}</div>
+          <div className="flipClockCards">
+            <div className="topHalfFlipCard">
+              <div>{hours}</div>
+            </div>
+            <div className={`topHalfAnimatedFlipCardCover ${hoursAnimation === false ? "animationNone" : ""} `} key={prevhours}>
+              <div>{prevhours}</div>
+            </div>
+            
+            <div className="bottomHalfFlipCard">
+              <div>{prevhours}</div>
+            </div>
+            <div
+              className={`bottomHalfAnimatedFlipCardCover ${hoursAnimation === false ? "animationNone" : ""}`}
+              onAnimationEnd={hourHandler}
+              key={hours}
+            >
+              <div>{hours}</div>
+            </div>
           </div>
+
+          <div className="flipClockCards">
+            <div className="topHalfFlipCard">
+              <div>{minutes}</div>
+            </div>
+            <div
+              className={`topHalfAnimatedFlipCardCover ${minutesAnimation === false ? "animationNone" : ""} `}
+              key={prevMinutes}
+            >
+              <div>{prevMinutes}</div>
+            </div>
+            <div className="bottomHalfFlipCard">
+              <div>{prevMinutes}</div>
+            </div>
+            <div
+              className={`bottomHalfAnimatedFlipCardCover ${minutesAnimation === false ? "animationNone" : ""}`}
+              onAnimationEnd={minutesHandler}
+              key={minutes}
+            >
+              <div>{minutes}</div>
+            </div>
+          </div>
+
+          <div className="flipClockCards">
+            <div className="topHalfFlipCard">
+              <div>{seconds}</div>
+            </div>
+            <div className="topHalfAnimatedFlipCardCover red" key={prevSecond}>
+              <div>{prevSecond}</div>
+            </div>
+            <div className="bottomHalfFlipCard">
+              <div>{prevSecond}</div>
+            </div>
+            <div className="bottomHalfAnimatedFlipCardCover red" onAnimationEnd={secondsHandler} key={seconds}>
+              <div>{seconds}</div>
+            </div>
+          </div>
+
           <div className="countDownCaption">Days</div>
           <div className="countDownCaption">Hours</div>
           <div className="countDownCaption">Minutes</div>
